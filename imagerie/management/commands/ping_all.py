5ca1948202bc40db3a0068bf72c54f2816908309
kligliro <kliglich.romuald@chu-amiens.fr>
2025-01-28 23:42:44 +0100

    modifs dans la commande ping_all

--- imagerie/management/commands/ping_all.py
+++ imagerie/management/commands/ping_all.py
@@ -8,20 +8,22 @@ class Command(BaseCommand):
     args = ''
     help = "Scanne les modalités pour noter celles qui répondent au ping et met à jour la BDD"
 
+    def horodat(self):
+        return timezone.now().strftime('%Y/%m/%d, %H:%M:%S')
+
     def handle(self, *args, **options):
-        # start_response('200 OK', [('Content-Type', 'text/plain')])
         import ping3
-        # modalites = Modalite.objects.filter(addrip__startswith="10.208.37.15").order_by('addrip')
+        # modalites = Modalite.objects.filter(addrip__startswith="10.208.37.1").order_by('addrip')
         modalites = Modalite.objects.all()  # .order_by('addrip')
-        count = 0
-        fic = Path(BASE_DIR / 'private/ping_all.txt')        
+        fic = Path(BASE_DIR / 'private/ping_all.txt')   
+        count = 0     
         with open(fic, "a") as result:
-            result.write(f"{timezone.now().strftime('%m/%d/%Y, %H:%M:%S')}")      
+            #result.write(f"{timezone.now().strftime('%Y/%m/%d, %H:%M:%S')}")      
             for modal in modalites:
-                # count += 1
+                count += 1
                 try:
                     res = ping3.ping(modal.addrip, timeout=1)
-                    # print("res = ", res, "   modal.ping = ", modal.ping)
+                    print(f"{count} : {modal.addrip}  ---->  {res} sec.                          ", flush=True, end="\r")
                     if res and (modal.ping == True) :                    
                         modal.recent_ping = timezone.now()
                         # print(f"---> {count} la modalité {modal.addrip} répond au ping et a déjà répondu au ping                                ", end="\x0d")
@@ -29,7 +31,7 @@ class Command(BaseCommand):
                         modal.ping = True
                         modal.first_ping = timezone.now()
                         # print(f"---> {count} la modalité {modal.addrip} répond pour la première fois au ping                                    ")
-                        result.write(f"{timezone.now()}  la modalite {modal.addrip} repond pour la premiere fois au ping\n")
+                        result.write(f"{self.horodat()} la modalite {modal.addrip} repond pour la premiere fois au ping\n")
                     elif (res == None) and (modal.ping == True) :
                         # print(f"---> {count} la modalité {modal.addrip} a déjà répondu pas au ping mais à l' air : éteinte? réformée? en panne? ")
                         pass
@@ -38,10 +40,12 @@ class Command(BaseCommand):
                         pass
                     modal.save()
                 except:
-                    pass
-                    # print(f"---> {count} la modalité {modal.addrip} a un problème d' accès !                                                ")
-            # print() 
+                    if "0.0.0" in modal.addrip:
+                        pass
+                    else:
+                        result.write(f"{self.horodat()} --> problème d' acces àla modalite : {modal.addrip} \n")
+                        # print(f"---> {count} la modalité {modal.addrip} a un problème d' accès !                                                ") 
 
             modal_ON = Modalite.objects.filter(ping=True).count()
-            modal_OFF = Modalite.objects.filter(ping=False).count()    
-            result.write(f" --> ping machines : {modal_ON} et {modal_OFF} qui ne repondent pas !\n")
+            modal_OFF = Modalite.objects.filter(ping=False).count()   
+            result.write(f"{self.horodat()} --> ON : {modal_ON}  ---  OFF : {modal_OFF} \n")
