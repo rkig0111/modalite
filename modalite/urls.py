b32c318abe765391f81f03575b9a4d3d2bfbde19
kligliro <kliglich.romuald@free.fr>
2025-03-28 17:54:46 +0100

    tests RESTAPI

--- modalite/urls.py
+++ modalite/urls.py
@@ -5,12 +5,13 @@ from django.views.generic import RedirectView
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf.urls.static import static
 from django.conf import settings
-from imagerie import views 
+from imagerie import views
 
 app_name = 'modalite'
 
 urlpatterns = [
     path("", include('imagerie.urls') ),
+    path("api-auth/", include('rest_framework.urls') ),
     path("admin/", admin.site.urls),
     path('signup', views.signup, name="signup"),
     path('login', views.login_user, name="login"),
