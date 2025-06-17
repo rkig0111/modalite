ae6f8ce6399b95c5811eb5d05b69bcd2761f7767
kligliro <kliglich.romuald@chu-amiens.fr>
2025-06-17 11:15:50 +0200

    correction pour le scheduler

--- imagerie/apps.py
+++ imagerie/apps.py
@@ -5,7 +5,6 @@ class ImagerieConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "imagerie"
 
-    # def ready(self):
-    #     from . import updater
-    #     if os.environ.get('RUN_MAIN'):
-    #         updater.start()
+    def ready(self):
+        from . import updater
+        updater.start()
